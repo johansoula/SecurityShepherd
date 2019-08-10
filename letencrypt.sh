9a9c8af9e4966af4e3a0f07b81365ef647ed53a3
Johan <johan.soula@sodexo.com>
2019-08-11 00:41:06 +0200

    fixing let encrypt missing flag

--- letencrypt.sh
+++ letencrypt.sh
@@ -1,3 +1,4 @@
+#set -e
 keyStoreFileName=shepherdKeystore.jks
 keystorePassword=sdxroxxnosecret
 FQDN="FIXME"
@@ -7,7 +8,7 @@ sudo apt update  -y
 sudo apt install  -y  certbot
 sudo service tomcat8 stop
 cd /home/*/ # Replace by the location of the JKS file if it doesn't work
-sudo certbot certonly  -d $FQDN --register-unsafely-without-email  --standalone --preferred-challenges http -n
+sudo certbot certonly  -d $FQDN --register-unsafely-without-email  --standalone --preferred-challenges http -n --agree-tos
 sudo cp ${keyStoreFileName} ${keyStoreFileName}.bak
 sudo keytool -delete -alias tomcat -keystore $keyStoreFileName -storepass $keystorePassword 
 sudo openssl pkcs12 -export -in /etc/letsencrypt/live/$FQDN/fullchain.pem -inkey /etc/letsencrypt/live/$FQDN/privkey.pem -out ${keyStoreFileName}.pkcs12 -name tomcat -passout pass:$keystorePassword
